2c30d0a4d3f794f6bf329f9c184c82dc34680493
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2019-11-15 01:34:23 +0700

    SuperMarket

---
+++ LoginForm/LoginForm/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.btnOK.Click += btnOK_Click;
+            this.btnCancel.Click += btnCancel_Click;
+        }
+
+        void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        void btnOK_Click(object sender, EventArgs e)
+        {
+            string name = this.txtName.Text;
+            string pass = this.txtPassword.Text;
+            if(name=="vlu" && pass=="vlu")
+            {
+                var form = new SMForm();
+                form.ShowDialog();
+                this.Close();
+            }
+            else if(name != "vlu")
+            {
+                MessageBox.Show("Account does not exist!!!");
+            }
+            else
+            {
+                MessageBox.Show("Wrong password!!!");
+            }
+        }
+    }
+}
---
+++ LoginForm/LoginForm/SMForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace LoginForm
+{
+    partial class SMForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.grdListItem = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnAddItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnEditItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDeleteItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.topToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alphabetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.grdListItem)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SuperMarketController";
+            // 
+            // grdListItem
+            // 
+            this.grdListItem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdListItem.Location = new System.Drawing.Point(12, 78);
+            this.grdListItem.Name = "grdListItem";
+            this.grdListItem.Size = new System.Drawing.Size(395, 233);
+            this.grdListItem.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(419, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAddItem,
+            this.btnEditItem,
+            this.btnDeleteItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // btnAddItem
+            // 
+            this.btnAddItem.Name = "btnAddItem";
+            this.btnAddItem.Size = new System.Drawing.Size(152, 22);
+            this.btnAddItem.Text = "Add item";
+            // 
+            // btnEditItem
+            // 
+            this.btnEditItem.Name = "btnEditItem";
+            this.btnEditItem.Size = new System.Drawing.Size(152, 22);
+            this.btnEditItem.Text = "Edit item";
+            // 
+            // btnDeleteItem
+            // 
+            this.btnDeleteItem.Name = "btnDeleteItem";
+            this.btnDeleteItem.Size = new System.Drawing.Size(152, 22);
+            this.btnDeleteItem.Text = "Delete item";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.topToolStripMenuItem,
+            this.alphabetToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // topToolStripMenuItem
+            // 
+            this.topToolStripMenuItem.Name = "topToolStripMenuItem";
+            this.topToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.topToolStripMenuItem.Text = "amount";
+            // 
+            // alphabetToolStripMenuItem
+            // 
+            this.alphabetToolStripMenuItem.Name = "alphabetToolStripMenuItem";
+            this.alphabetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.alphabetToolStripMenuItem.Text = "alphabet";
+            // 
+            // SMForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 323);
+            this.Controls.Add(this.grdListItem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "SMForm";
+            this.Text = "SMForm";
+            ((System.ComponentModel.ISupportInitialize)(this.grdListItem)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView grdListItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnAddItem;
+        private System.Windows.Forms.ToolStripMenuItem btnEditItem;
+        private System.Windows.Forms.ToolStripMenuItem btnDeleteItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem topToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem alphabetToolStripMenuItem;
+    }
+}
