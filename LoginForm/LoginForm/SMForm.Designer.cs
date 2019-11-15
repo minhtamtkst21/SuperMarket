29d7a1c822ae6fe6b3dba67bfcf736e8cc463cdf
minhtamtkst21 <55739252+minhtamtkst21@users.noreply.github.com>
2019-11-15 13:24:26 +0700

    supermarket

--- LoginForm/LoginForm/SMForm.Designer.cs
+++ LoginForm/LoginForm/SMForm.Designer.cs
@@ -29,111 +29,210 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.grdListItem = new System.Windows.Forms.DataGridView();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.btnAddItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.btnEditItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.btnDeleteItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.topToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.alphabetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.grdListItem)).BeginInit();
-            this.menuStrip1.SuspendLayout();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.textBox22 = new System.Windows.Forms.TextBox();
+            this.textBox24 = new System.Windows.Forms.TextBox();
+            this.textBox26 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(81, 41);
+            this.label1.Location = new System.Drawing.Point(82, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(228, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "SuperMarketController";
             // 
-            // grdListItem
+            // textBox1
             // 
-            this.grdListItem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grdListItem.Location = new System.Drawing.Point(12, 78);
-            this.grdListItem.Name = "grdListItem";
-            this.grdListItem.Size = new System.Drawing.Size(395, 233);
-            this.grdListItem.TabIndex = 1;
+            this.textBox1.Location = new System.Drawing.Point(8, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.TabIndex = 1;
             // 
-            // menuStrip1
+            // textBox2
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.editToolStripMenuItem,
-            this.viewToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(419, 24);
-            this.menuStrip1.TabIndex = 2;
-            this.menuStrip1.Text = "menuStrip1";
+            this.textBox2.Location = new System.Drawing.Point(140, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 20);
+            this.textBox2.TabIndex = 2;
             // 
-            // editToolStripMenuItem
+            // textBox3
             // 
-            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnAddItem,
-            this.btnEditItem,
-            this.btnDeleteItem});
-            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
-            this.editToolStripMenuItem.Text = "Edit";
+            this.textBox3.Location = new System.Drawing.Point(272, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(126, 20);
+            this.textBox3.TabIndex = 3;
             // 
-            // btnAddItem
+            // textBox4
             // 
-            this.btnAddItem.Name = "btnAddItem";
-            this.btnAddItem.Size = new System.Drawing.Size(152, 22);
-            this.btnAddItem.Text = "Add item";
+            this.textBox4.Location = new System.Drawing.Point(8, 123);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(126, 20);
+            this.textBox4.TabIndex = 4;
             // 
-            // btnEditItem
+            // textBox5
             // 
-            this.btnEditItem.Name = "btnEditItem";
-            this.btnEditItem.Size = new System.Drawing.Size(152, 22);
-            this.btnEditItem.Text = "Edit item";
+            this.textBox5.Location = new System.Drawing.Point(141, 122);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 20);
+            this.textBox5.TabIndex = 5;
             // 
-            // btnDeleteItem
+            // textBox6
             // 
-            this.btnDeleteItem.Name = "btnDeleteItem";
-            this.btnDeleteItem.Size = new System.Drawing.Size(152, 22);
-            this.btnDeleteItem.Text = "Delete item";
+            this.textBox6.Location = new System.Drawing.Point(273, 122);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(125, 20);
+            this.textBox6.TabIndex = 6;
             // 
-            // viewToolStripMenuItem
+            // textBox7
             // 
-            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.topToolStripMenuItem,
-            this.alphabetToolStripMenuItem});
-            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
-            this.viewToolStripMenuItem.Text = "View";
+            this.textBox7.Location = new System.Drawing.Point(8, 199);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(126, 20);
+            this.textBox7.TabIndex = 7;
             // 
-            // topToolStripMenuItem
+            // textBox8
             // 
-            this.topToolStripMenuItem.Name = "topToolStripMenuItem";
-            this.topToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.topToolStripMenuItem.Text = "amount";
+            this.textBox8.Location = new System.Drawing.Point(141, 198);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(125, 20);
+            this.textBox8.TabIndex = 8;
             // 
-            // alphabetToolStripMenuItem
+            // textBox9
             // 
-            this.alphabetToolStripMenuItem.Name = "alphabetToolStripMenuItem";
-            this.alphabetToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
-            this.alphabetToolStripMenuItem.Text = "alphabet";
+            this.textBox9.Location = new System.Drawing.Point(273, 198);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(125, 20);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(59, 75);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(75, 20);
+            this.textBox10.TabIndex = 10;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(191, 75);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(75, 20);
+            this.textBox12.TabIndex = 10;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(323, 75);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(75, 20);
+            this.textBox14.TabIndex = 10;
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(59, 148);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(75, 20);
+            this.textBox16.TabIndex = 10;
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(191, 148);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(75, 20);
+            this.textBox17.TabIndex = 10;
+            // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(323, 148);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(75, 20);
+            this.textBox19.TabIndex = 10;
+            // 
+            // textBox22
+            // 
+            this.textBox22.Location = new System.Drawing.Point(323, 224);
+            this.textBox22.Name = "textBox22";
+            this.textBox22.Size = new System.Drawing.Size(75, 20);
+            this.textBox22.TabIndex = 10;
+            // 
+            // textBox24
+            // 
+            this.textBox24.Location = new System.Drawing.Point(191, 224);
+            this.textBox24.Name = "textBox24";
+            this.textBox24.Size = new System.Drawing.Size(75, 20);
+            this.textBox24.TabIndex = 10;
+            // 
+            // textBox26
+            // 
+            this.textBox26.Location = new System.Drawing.Point(59, 225);
+            this.textBox26.Name = "textBox26";
+            this.textBox26.Size = new System.Drawing.Size(75, 20);
+            this.textBox26.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "ViewList";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 277);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // SMForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(419, 323);
-            this.Controls.Add(this.grdListItem);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox26);
+            this.Controls.Add(this.textBox24);
+            this.Controls.Add(this.textBox22);
+            this.Controls.Add(this.textBox19);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "SMForm";
             this.Text = "SMForm";
-            ((System.ComponentModel.ISupportInitialize)(this.grdListItem)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -142,14 +241,25 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView grdListItem;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem btnAddItem;
-        private System.Windows.Forms.ToolStripMenuItem btnEditItem;
-        private System.Windows.Forms.ToolStripMenuItem btnDeleteItem;
-        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem topToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem alphabetToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.TextBox textBox22;
+        private System.Windows.Forms.TextBox textBox24;
+        private System.Windows.Forms.TextBox textBox26;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
