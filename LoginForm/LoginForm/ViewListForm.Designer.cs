5a425657ff559cb89467f0aad1abc9b79c49ba37
minhtam <minhtamtruongkhanh.st@gmail.com>
2019-11-20 17:31:17 +0700

    SuperMarket

---
+++ LoginForm/LoginForm/ViewListForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace LoginForm
+{
+    partial class ViewListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addItemToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewItemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aZToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.numberRemainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.iDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(284, 191);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addItemToolStripMenuItem,
+            this.viewItemToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addItemToolStripMenuItem
+            // 
+            this.addItemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addItemToolStripMenuItem1,
+            this.deleteItemToolStripMenuItem,
+            this.editItemToolStripMenuItem});
+            this.addItemToolStripMenuItem.Name = "addItemToolStripMenuItem";
+            this.addItemToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.addItemToolStripMenuItem.Text = "Edit item";
+            // 
+            // addItemToolStripMenuItem1
+            // 
+            this.addItemToolStripMenuItem1.Name = "addItemToolStripMenuItem1";
+            this.addItemToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.addItemToolStripMenuItem1.Text = "Add item";
+            // 
+            // deleteItemToolStripMenuItem
+            // 
+            this.deleteItemToolStripMenuItem.Name = "deleteItemToolStripMenuItem";
+            this.deleteItemToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deleteItemToolStripMenuItem.Text = "Delete item";
+            // 
+            // editItemToolStripMenuItem
+            // 
+            this.editItemToolStripMenuItem.Name = "editItemToolStripMenuItem";
+            this.editItemToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.editItemToolStripMenuItem.Text = "Edit item";
+            // 
+            // viewItemToolStripMenuItem
+            // 
+            this.viewItemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aZToolStripMenuItem,
+            this.numberRemainToolStripMenuItem,
+            this.iDToolStripMenuItem});
+            this.viewItemToolStripMenuItem.Name = "viewItemToolStripMenuItem";
+            this.viewItemToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.viewItemToolStripMenuItem.Text = "View item";
+            // 
+            // aZToolStripMenuItem
+            // 
+            this.aZToolStripMenuItem.Name = "aZToolStripMenuItem";
+            this.aZToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.aZToolStripMenuItem.Text = "A-Z";
+            // 
+            // numberRemainToolStripMenuItem
+            // 
+            this.numberRemainToolStripMenuItem.Name = "numberRemainToolStripMenuItem";
+            this.numberRemainToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.numberRemainToolStripMenuItem.Text = "Number remain";
+            // 
+            // iDToolStripMenuItem
+            // 
+            this.iDToolStripMenuItem.Name = "iDToolStripMenuItem";
+            this.iDToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.iDToolStripMenuItem.Text = "ID";
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(13, 226);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(75, 23);
+            this.btnHome.TabIndex = 4;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(183, 225);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // ViewListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "ViewListForm";
+            this.Text = "ViewListForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addItemToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem deleteItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewItemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aZToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem numberRemainToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem iDToolStripMenuItem;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ LoginForm/LoginForm/ViewListForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class ViewListForm : Form
+    {
+        public ViewListForm()
+        {
+            InitializeComponent();
+            this.btnClose.Click += btnClose_Click;
+            this.btnHome.Click += btnHome_Click;
+        }
+
+        void btnHome_Click(object sender, EventArgs e)
+        {
+            var main = new SMForm();
+            main.ShowDialog();
+        }
+
+        void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
